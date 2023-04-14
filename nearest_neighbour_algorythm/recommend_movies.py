363f419f0d264de0eb871ab1a4515fe6966791e1
QBeukelman <98453355+QBeukelman@users.noreply.github.com>
2023-04-14 14:19:23 +0200

    saving

--- nearest_neighbour_algorythm/recommend_movies.py
+++ nearest_neighbour_algorythm/recommend_movies.py
@@ -6,7 +6,7 @@
 #    By: quentinbeukelman <quentinbeukelman@stud      +#+                      #
 #                                                    +#+                       #
 #    Created: 2023/04/12 14:50:09 by quentinbeuk   #+#    #+#                  #
-#    Updated: 2023/04/12 16:22:17 by quentinbeuk   ########   odam.nl          #
+#    Updated: 2023/04/14 14:18:44 by quentinbeuk   ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,30 +15,38 @@ import sys
 from termcolor import colored
 
 # ==============================================================================: RECOMMEND
-def ft_recommend_list(movie_id):
+def ft_recommend_list(movie_id, n=30, threshold=0.7):
     with open('../data/cosign_similarity_matrix.csv', 'r') as file:
         reader = csv.reader(file)
         header = next(reader)  # Skip header row
         similarity_scores = {}
-        
+
         for row in reader:
             if row[0] == movie_id:
                 for i in range(1, len(row)):
                     similarity_scores[header[i]] = float(row[i])
                 break
 
-    sorted_scores = sorted(similarity_scores.items(), key=lambda x: x[1], reverse=True)
-    top_movies = [movie[0] for movie in sorted_scores[:10]]
-    
+    sorted_scores = ft_sort_scores(similarity_scores, threshold)
+    top_movies = [movie[0] for movie in sorted_scores[:n]]
+
     if movie_id in top_movies:
         top_movies.remove(movie_id)  # remove searched movie from recommended movies
-    sim_values = [movie[1] for movie in sorted_scores[:10]]
-    
+    sim_values = [movie[1] for movie in sorted_scores[:n]]
+
     if movie_id in top_movies:
         sim_values.remove(similarity_scores[movie_id])  # remove similarity score for searched movie
 
     return top_movies, sim_values
 
+
+def ft_sort_scores(similarity_scores, threshold):
+    weighted_scores = {}
+    for movie, score in similarity_scores.items():
+        weighted_scores[movie] = score
+    sorted_scores = sorted(weighted_scores.items(), key=lambda x: x[1], reverse=True)
+    sorted_scores = [(movie, score) for movie, score in sorted_scores if score >= threshold]
+    return sorted_scores
 
 # ==============================================================================: ID TO TITLE
 def ft_id_to_title(movie_ids):
